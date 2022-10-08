a4163c7347b3d70790d17f791c0678fd0bc8087b
raisa098 <115281327+raisa098@users.noreply.github.com>
2022-10-09 02:44:50 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,9 +43,9 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/AstroMusikk")
+    "SUPPORT_CHANNEL", "https://t.me/Idnyahilang")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/AstroMusikk")
+    "SUPPORT_GROUP", "https://Wilonacuntrikk")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
